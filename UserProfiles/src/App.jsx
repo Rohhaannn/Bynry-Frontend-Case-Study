2bdd84e9ac7ae1754edef9fabc922e842a3af871
Rohan <shingaderohan96@gmail.com>
2024-04-21 00:42:29 +0530

    All components moved to main

--- UserProfiles/src/App.jsx
+++ UserProfiles/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import ProfileList from './components/ProfileList'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
       <div>
-        <ProfileList/>
+        
       </div>
     </>
   )
