d90c2ddfa9a5f5e27fc6bbcfa39d00373da201d4
Rohan <shingaderohan96@gmail.com>
2024-04-21 21:16:35 +0530

    Home, card 7 profile components added

--- UserProfiles/src/components/ProfileList.jsx
+++ UserProfiles/src/components/ProfileList.jsx
@@ -1,39 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-
-
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Card from './Card'
 
 const ProfileList = () => {
-  const [profiles, setProfiles] = useState([]);
+  const [users, setUsers] = useState([]);
 
-  useEffect(()=>{
-    try {
-      fetch('https://jsonplaceholder.typicode.com/users')
-        .then(res => res.json())
-        .then(data => setProfiles(data))
-    } catch (error) {
-        console.error('Error Fetching Data:', error)
-    }
-  },[])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        return response.json();
+      })
+      .then(data => setUsers(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
 
   return (
-    <div className='container mx-auto py-8'>
-      <h1 className='text-3xl font-bold mb-4'> Profiles </h1>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-        {
-          profiles.map(profile => (
-            <div key={profile.id} className='bg-white p-4 rounded-lg shadow'>
-              <Link to={`/profile/${profile.id}`}>
-                <h2 className='text-xl font-bold mb-2'>{profile.name}</h2>
-                <img/>
-                <p className="text-gray-600">{profile.company}</p>
-              </Link>
-            </div>
-          ))
-        }
-
-      </div>
+    <div className="max-w-screen-xl container mx-auto py-8">
+    <h1 className="text-3xl font-bold mb-4 text-center items-center">Users</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      {users.map(user => (
+        <div key={user.id} className="bg-white p-4 rounded-lg shadow">
+          <Link to={`/profile/${user.id}`}>
+            <Card user={user}/>
+          </Link>
+        </div>
+      ))}
     </div>
+  </div>
   )
 }
 
