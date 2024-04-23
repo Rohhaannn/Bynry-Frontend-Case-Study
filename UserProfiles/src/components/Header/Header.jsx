fe7dc523b814ddf91265d96a7f134c8a3a301967
Rohan <shingaderohan96@gmail.com>
2024-04-23 18:20:25 +0530

    Routing reload issue resolved

--- UserProfiles/src/components/Header/Header.jsx
+++ UserProfiles/src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import { Menu, X } from 'lucide-react'
 
 const menuItems = [
@@ -50,12 +51,12 @@ const Header = () => {
             <ul className="inline-flex space-x-8">
               {menuItems.map((item) => (
                 <li key={item.name}>
-                  <a
-                    href={item.href}
+                  <Link
+                    to={item.href}
                     className="text-sm font-semibold text-gray-800 hover:text-gray-900"
                   >
                     {item.name}
-                  </a>
+                  </Link>
                 </li>
               ))}
             </ul>
@@ -107,15 +108,15 @@ const Header = () => {
                   <div className="mt-6">
                     <nav className="grid gap-y-4">
                       {menuItems.map((item) => (
-                        <a
+                        <Link
                           key={item.name}
-                          href={item.href}
+                          to={item.href}
                           className="-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-gray-50"
                         >
                           <span className="ml-3 text-base font-medium text-gray-900">
                             {item.name}
                           </span>
-                        </a>
+                        </Link>
                       ))}
                     </nav>
                   </div>
@@ -123,7 +124,7 @@ const Header = () => {
                     type="button"
                     className="mt-4 w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                   >
-                    Button text
+                    Logout
                   </button>
                 </div>
               </div>
