9529bf55957223b361b15846c98a28d8aaf84d98
Rohan <shingaderohan96@gmail.com>
2024-04-23 21:00:32 +0530

    App Logo & title logo changed with new image

---
+++ UserProfiles/src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../../public/logo.png'
+
+const Logo = () => {
+  return (
+    <div>
+      <img src={logo} width={30} height={30}/>
+    </div>
+  )
+}
+
+export default Logo
