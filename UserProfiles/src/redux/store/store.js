ab555ffb555737fe3bba4660d6836d6df98f8ec9
Rohan <shingaderohan96@gmail.com>
2024-04-26 12:50:43 +0530

    store issue resolved

--- UserProfiles/src/redux/store/store.js
+++ UserProfiles/src/redux/store/store.js
@@ -1,6 +1,6 @@
-import { createStore, applyMiddleware } from 'react-redux';
-import thunk from 'react-redux';
-import rootReducer from '../reducers/userReducer';
+import { createStore, applyMiddleware } from 'redux'; 
+import { thunk } from 'redux-thunk'; 
+import rootReducer from '../reducers/userReducer'; 
 
 const initialState = {
   users: {
