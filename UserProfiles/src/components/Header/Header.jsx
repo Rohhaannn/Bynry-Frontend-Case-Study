30cc60e759df4d0faf448efbcc40ef8c27c98119
Rohan <shingaderohan96@gmail.com>
2024-04-28 23:08:34 +0530

    trying to solve header glitch

--- UserProfiles/src/components/Header/Header.jsx
+++ UserProfiles/src/components/Header/Header.jsx
@@ -1,45 +1,57 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Menu, X } from "lucide-react";
+import { Link } from "react-router-dom";
 import Logo from "../Logo";
 
-const Header = () => {
-  return (
-    <header className="shadow sticky z-50 top-0">
-      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <Link to="/" className="flex items-center">
-            <Logo/>
-            <span className="ml-4 text-lg font-bold"> Profiles</span>
-          </Link>
-          <div className="flex items-center lg:order-2">
-            <Link
-              to="/admin"
-              className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-            >
-              Admin
-            </Link>
-            <Link
-              to="#"
-              className="text-white bg-[#3977d2] hover:bg-blue-600 hover:text-black focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-            >
-              Get started
-            </Link>
-          </div>
-          <div
-            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-            id="mobile-menu-2"
-          >
-            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+const menuItems = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "All Users",
+    href: "/profilelist",
+  },
+  {
+    name: "Search User",
+    href: "/searchuser",
+  },
+  {
+    name: "Contact",
+    href: "/contact",
+  },
+  {
+    name: "Admin",
+    href: "/admin",
+  },
+];
 
-              {/* =================== Home =================== */}
-              <li>
-                <NavLink
-                  to="/"
-                  className={({isActive}) =>
-                      `block py-2 pr-4 
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className="relative w-full bg-white">
+      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
+        <div className="inline-flex items-center space-x-2">
+          <span>
+            <Logo />
+          </span>
+          <span className="font-bold">Profiles</span>
+        </div>
+        <div className="hidden lg:block">
+          <ul className="inline-flex space-x-8">
+            {menuItems.map((item) => (
+              <li key={item.name}>
+                <Link
+                  to={item.href}
+                  className={({ isActive }) =>
+                    `block py-2 pr-4 
                       pl-3 duration-200
                       ${isActive ? "text-[#3977d2]" : "text-gray-700"}
-                      border-b 
                       border-gray-100 
                       hover:bg-gray-50 
                       lg:hover:bg-transparent 
@@ -48,104 +60,86 @@ const Header = () => {
                       lg:p-0`
                   }
                 >
-                  Home
-                </NavLink>
+                  {item.name}
+                </Link>
               </li>
-
-              {/* =================== All Users =================== */}
-              <li>
-                <NavLink
-                  to= "/profilelist"
-                  className={({isActive}) =>
-                      `block py-2 pr-4 
-                      pl-3 duration-200
-                      ${isActive ? "text-[#3977d2]" : "text-gray-700"}
-                      border-b 
-                      border-gray-100 
-                      hover:bg-gray-50 
-                      lg:hover:bg-transparent 
-                      lg:border-0 
-                      hover:text-[#3977d2] 
-                      lg:p-0`
-                  }
-                >
-                  All Profiles
-                </NavLink>
-              </li>
-
-              {/* =================== Search User =================== */}
-              <li>
-                <NavLink
-                  to= "/searchuser"
-                  className={({isActive}) =>
-                      `block py-2 pr-4 
-                      pl-3 duration-200
-                      ${isActive ? "text-[#3977d2]" : "text-gray-700"}
-                      border-b 
-                      border-gray-100 
-                      hover:bg-gray-50 
-                      lg:hover:bg-transparent 
-                      lg:border-0 
-                      hover:text-[#3977d2]  
-                      lg:p-0`
-                  }
-                >
-                  Search User
-                </NavLink>
-              </li>
-
-
-              {/* =================== Contact =================== */}
-              <li>
-                <NavLink
-                  to= "/contact"
-                  className={({isActive}) =>
-                      `block py-2 pr-4 
-                      pl-3 duration-200
-                      ${isActive ? "text-[#3977d2]" : "text-gray-700"}
-                      border-b 
-                      border-gray-100 
-                      hover:bg-gray-50 
-                      lg:hover:bg-transparent 
-                      lg:border-0 
-                      hover:text-[#3977d2] 
-                      lg:p-0`
-                  }
-                >
-                  Contact
-                </NavLink>
-              </li>
-
-              {/* =================== Admin =================== */}
-              {/* <li>
-                <NavLink
-                  to= "/admin"
-                  className={({isActive}) =>
-                      `block py-2 pr-4 
-                      pl-3 duration-200
-                      ${isActive ? "text-[#3977d2]" : "text-gray-700"}
-                      border-b 
-                      border-gray-100 
-                      hover:bg-gray-50 
-                      lg:hover:bg-transparent 
-                      lg:border-0 
-                      hover:text-[#3977d2] 
-                      lg:p-0`
-                  }
-                >
-                  Admin
-                </NavLink>
-              </li> */}
-
-              
-              
-
-            </ul>
-          </div>
+            ))}
+          </ul>
         </div>
-      </nav>
-    </header>
+        <div className="hidden lg:block">
+          <button
+            type="button"
+            className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+          >
+            Login
+          </button>
+        </div>
+        <div className="lg:hidden">
+          <Menu onClick={toggleMenu} className="h-6 w-6 cursor-pointer" />
+        </div>
+        {isMenuOpen && (
+          <div className="absolute inset-x-0 top-0 z-50 origin-top-right transform p-2 transition lg:hidden">
+            <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+              <div className="px-5 pb-6 pt-5">
+                <div className="flex items-center justify-between">
+                  <div className="inline-flex items-center space-x-2">
+                    <span>
+                      <Logo />
+                    </span>
+                  </div>
+                  <div className="-mr-2">
+                    <button
+                      type="button"
+                      onClick={toggleMenu}
+                      className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                    >
+                      <span className="sr-only">Close menu</span>
+                      <X className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                  </div>
+                </div>
+                <div className="mt-6">
+                  <nav className="grid gap-y-4">
+                    {menuItems.map((item) => (
+                      <Link
+                        key={item.name}
+                        to={item.href}
+                        className="-m-3 flex items-center rounded-md p-3 text-sm font-semibold hover:bg-gray-50"
+                      >
+                        <span className="ml-3 text-base font-medium text-gray-900">
+                          {item.name}
+                        </span>
+                      </Link>
+                    ))}
+                  </nav>
+                </div>
+                {/* <button
+                  type="button"
+                  className="mt-4 w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                >
+                  Login
+                </button> */}
+                <div className="flex items-center lg:order-2">
+                  <Link
+                    to="/admin"
+                    className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                  >
+                    Admin
+                  </Link>
+                  <Link
+                    to="#"
+                    className="text-white bg-[#3977d2] hover:bg-blue-600 hover:text-black focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                  >
+                    Get started
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   );
-}
+};
 
-export default Header
+export default Header;
